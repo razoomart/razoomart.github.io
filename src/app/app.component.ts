af65dcc36101c27f0e003cded6bc43f991470fef
nnabse <nnabse@gmail.com>
2023-01-18 04:00:20 +0300

    add fonts, images, icons, main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public headerSrc = 'assets/icons/razoomart-logo.svg';
+
+  public headerSrcList: string[] = [
+    'assets/icons/razoomart-logo3.svg',
+    'assets/icons/razoomart-logo4.svg',
+    'assets/icons/razoomart-logo5.svg',
+    'assets/icons/razoomart-logo6.svg',
+  ];
+
+  getNewHeader(): void {
+    this.headerSrc =
+      this.headerSrcList[Math.floor(Math.random() * this.headerSrcList.length)];
+  }
+}
