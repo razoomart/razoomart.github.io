0dc4ffe3ebfd0e6e6ac44e05dd58f22902c06968
nnabse <nnabse@gmail.com>
2023-01-25 18:04:29 +0300

    add team page layout, add memojis

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { IconList } from '@constants/icons.constants';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss'],
 })
-export class TeamComponent {}
+export class TeamComponent {
+  public icons = IconList;
+}
