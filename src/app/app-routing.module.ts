0d28e3f6b0ddd34572b7c142aa141c9e2e962778
nnabse <nnabse@gmail.com>
2023-03-19 20:52:13 +0300

    add pages titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from '@pages/main/main.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent },
+  { path: '', component: MainComponent, title: 'Студия Razoom.art' },
   {
     path: 'team',
     loadChildren: () =>
       import('./pages/team/team.module').then((m) => m.TeamModule),
+    title: 'О команде Razoom.art',
   },
   {
     path: 'cases',
     loadChildren: () =>
       import('./pages/cases/cases.module').then((m) => m.CasesModule),
+    title: 'Кейсы Razoom.art',
   },
   {
     path: 'vw',
     loadChildren: () => import('./pages/vw/vw.module').then((m) => m.VwModule),
+    title: 'vw–px конвертер',
   },
   {
     path: '404',
@@ -25,6 +28,7 @@ const routes: Routes = [
       import('./pages/not-found/not-found.module').then(
         (m) => m.NotFoundModule
       ),
+    title: '404',
   },
   { path: '**', pathMatch: 'full', redirectTo: '404' },
 ];
